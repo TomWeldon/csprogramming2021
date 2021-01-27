6714e05840fc04fa9960b09ffc28217e7d3209c2
TomWeldon <weldontom@eircom.net>
2021-01-27 22:36:15 +0000

    Week 2 task

--- bmi.py
+++ bmi.py
@@ -12,4 +12,4 @@ bmi = wt / ((ht/100)**2)
 #Confirmation of input of height in cm and weight in kg
 #Output BMI to two decinal places
 print('Your height is {}cm and your weight is {}kg'.format(ht, wt))
-print('Your bmi is {:.2f}'.format(bmi))
+print('Your BMI is {:.2f}'.format(bmi))
