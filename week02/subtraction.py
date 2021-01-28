dbf3553c6ba0a9612fc6824b397395b44c887011
TomWeldon <weldontom@eircom.net>
2021-01-28 20:37:57 +0000

    code modified

--- week02/subtraction.py
+++ week02/subtraction.py
@@ -1,10 +1,8 @@
 #Print result of 21 - 4
 #Tom weldon:
 
-#Assign subration to result of 21 - 4
-subtraction = 21 - 4
 
-#print the result
-print(subtraction)
+#print the result of 21 - 4
+print(21 - 4)
 
 
