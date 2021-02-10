2e4e5bb13b9f38898b0387e95eece44e0b648cf3
TomWeldon <weldontom@eircom.net>
2021-02-10 14:01:14 +0000

    code modified

---
+++ week03/sub.py
@@ -0,0 +1,12 @@
+#Program to subtract one number from another
+#Author: Tom Weldon G00398828
+
+#input reads in a string and it's converted to an int
+#Mathematical operation is then carrie out
+
+x = int(input('Enter first number: '))
+y = int(input('Enter second number: '))
+
+answer = x - y
+
+print('{} minus {} is {}'.format(x, y ,answer))
---
+++ week03/testTypes.py
@@ -0,0 +1,14 @@
+#Printing variable types and value
+#Author: Tom Weldon G00398828
+
+i = 2
+fl = 3.5
+isa = True
+memo = 'How Now Brown Cow'
+lots = []
+
+print('varaiable {} is of type:{} and value {}'.format('i', type(i), i))
+print('varaiable {} is of type:{} and value {}'.format('fl', type(fl), fl))
+print('varaiable {} is of type:{} and value {}'.format('isa', type(isa), isa))
+print('varaiable {} is of type:{} and value {}'.format('memo', type(memo), memo))
+print('varaiable {} is of type:{} and value {}'.format('lots', type(lots), lots))
