5e563c2c0d3d91288adbe9129d7462683be7ebf0
TomWeldon <weldontom@eircom.net>
2021-02-14 00:55:28 +0000

    'Week04'

--- Week04/guess.py
+++ Week04/guess.py
@@ -2,9 +2,13 @@
 # a number until the correct numvber is inputted
 # Author: Tom Weldon G00398828
 
+# Number to guess
 numberToGuess = 30
+# Request to input a number
 guess = int(input('Please enter a number: '))
 
+# While loop when guess is not equal to numberToGuess
+# if statement for advising guess is too low or too high
 while guess != numberToGuess:
     print('Wrong')
     if guess < numberToGuess:
@@ -13,4 +17,6 @@ while guess != numberToGuess:
     else: 
         print('Your number {} was too high'.format(guess))    
         guess = int(input('Please guess again: '))
+
+# Out put to screen if guess is equal to numberToGuess
 print('Well done! yes the number was {}'.format(numberToGuess))
