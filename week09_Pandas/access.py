844c2eb91e7509d97693605eca2150d9f5f99346
TomWeldon <weldontom@eircom.net>
2021-03-24 16:03:47 +0000

    week09

--- week09_Pandas/access.py
+++ week09_Pandas/access.py
@@ -2,13 +2,13 @@ import pandas as pd
 import re
 import matplotlib.pyplot as plt
 
-logFilename = 'access1.csv'
+logFilename = 'access.log'
 
 
 
 colNames = ('ip','dash1','userId','time','url','status code','size of response','referer','user agent','dunno')
 df = pd.read_csv(logFilename,sep=' ', header=None, names=colNames)
-df.drop(columns=['dash1','userId'], inplace=True)
+df.drop(columns=['dash1','userId','dunno'], inplace=True)
 #get rid of [] i 'time'
 def getNewValue(x):
     newvalue = re.search('[\w:/]+',x).group()
@@ -20,18 +20,19 @@ def sessionId(x):
 # Making new column and populating it
 df['session ID'] = df['url'].apply(sessionId)
 
-df['time'] = df['time'].apply(getNewValue)
+#df['time'] = df['time'].apply(getNewValue)
 #chamge 'time' format
-df['time']= pd.to_datetime(df['time'], format='%d/%b/%Y:%H:%M:%S')
+#df['time']= pd.to_datetime(df['time'], format='%d/%b/%Y:%H:%M:%S')
 # Set time to be index
-df = df.set_index(['time'])
+#df = df.set_index(['time'])
 excelFilename = 'access.xlsx'
 df.to_excel(excelFilename, index=True, sheet_name='data')
 #summary = df['size of response'].sum()
 summary = df.groupby('session ID', as_index=False).agg({'size of response': 'sum'})
+summary = summary.sort_values('size of response', ascending = False).head(10)
 print(summary)
-summary = 'aggSessionID.xlsx'
-df.to_excel(summary, index=True,sheet_name='aggregate')
+#summary = 'aggSessionID.xlsx'
+#df.to_excel(summary, index=True,sheet_name='aggregate')
 #print(df.iloc[1])
 #print(df.dtypes)
 
